bfbf124e47bdb3349a642422de2de63ddc847210
Sibdever <sibdever@gmail.com>
2020-05-21 22:48:59 +0700

    Read request params

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -3,6 +3,12 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.*;
 
 public class MainController {
 
@@ -15,12 +21,14 @@ public class MainController {
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
 
         server.createContext("/api/hello", (exchange -> {
-            String respText = "Hello!";
-            BufferedReader input = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
-            String requestText = input.readLine();
+
+            Map<String, List<String>> params = splitQuery(exchange.getRequestURI().getRawQuery());
+            String noNameText = "Anonymous";
+            String name = params.getOrDefault("name", List.of(noNameText)).stream().findFirst().orElse(noNameText);
+            String respText = String.format("Hello %s!", name);
+
             System.err.println("GET USER INPUT");
-            if(!requestText.isEmpty())
-                respText = requestText;
+
             exchange.sendResponseHeaders(200, respText.getBytes().length);
             OutputStream output = exchange.getResponseBody();
             output.write(respText.getBytes());
@@ -31,4 +39,16 @@ public class MainController {
         server.setExecutor(null); // creates a default executor
         server.start();
     }
+
+    public static Map<String, List<String>> splitQuery(String query) {
+        if (query == null || "".equals(query)) {
+            return Collections.emptyMap();
+        }
+
+        return Pattern.compile("&").splitAsStream(query)
+                .map(s -> Arrays.copyOf(s.split("="), 2))
+                .collect(groupingBy(s -> s[0], mapping(s -> s[1], toList())));
+
+    }
+
 }
