b024365873981880e96d858f4138da11187872bb
Sibdever <sibdever@gmail.com>
2020-05-23 18:13:42 +0700

    Complete testing with console client mode

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,13 +1,16 @@
 import api.ServerCommand;
+import com.alibaba.fastjson.JSONArray;
 import exceptions.NoResourceInitException;
 import model.ExternalCell;
 import model.Field;
 import model.Pair;
 import score.ScoreItem;
 import score.ScoreManager;
+import serialization.Serializer;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalTime;
 import java.util.TreeSet;
 
 public class ClientHandler extends Thread {
@@ -48,7 +51,7 @@ public class ClientHandler extends Thread {
         try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream())) {
 
-            String info = "";
+            String info;
 
             while ((info = input.readLine()) != null) {
                 String[] args = info.split(" ");
@@ -59,12 +62,9 @@ public class ClientHandler extends Thread {
                     case GAME_HELP -> objectOutputStream.writeUTF(GAME_INFO);
                     case NEW_GAME -> {
                         field = new Field(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-                        objectOutputStream.writeUTF(ExternalCell.tableToJson(field.getExternalCells()));
+                        objectOutputStream.writeUTF(Serializer.externalToJson(field.getExternalCells()));
                     }
-                    case HIGH_SCORE -> {
-                        TreeSet<ScoreItem> scoreItems = new ScoreManager().getScoreTable();
-                        objectOutputStream.writeObject(scoreItems);
-                    }
+                    case HIGH_SCORE -> objectOutputStream.writeUTF(Serializer.scoreTableToJson(new ScoreManager().getScoreTable()));
                     // Todo test
                     case CHECK -> {
                         if (field != null) {
@@ -76,20 +76,20 @@ public class ClientHandler extends Thread {
                                     System.err.println();
                                 }
 
-                                objectOutputStream.writeUTF(ExternalCell.tableToJson(field.getExternalCells()));
-                            } else objectOutputStream.writeUTF("{}");
+                                objectOutputStream.writeUTF(Serializer.externalToJson(field.getExternalCells()));
+                            } else objectOutputStream.writeUTF(new JSONArray().toJSONString());
                         }
                     }
                     // Todo test
                     case SHOW_FIELD -> {
                         if (field != null) {
-                            objectOutputStream.writeUTF(ExternalCell.tableToJson(field.getExternalCells()));
+                            objectOutputStream.writeUTF(Serializer.externalToJson(field.getExternalCells()));
                         }
                     }
                     // Todo test
                     case GET_MARKS -> {
                         if (field != null) {
-                            objectOutputStream.writeObject(new Pair<>(field.getMarks(), field.getMarksLimit()));
+                            objectOutputStream.writeUTF(Serializer.pairToJson(new Pair<>(field.getMarks(), field.getMarksLimit())));
                         }
                     }
                     // Todo test
@@ -97,10 +97,23 @@ public class ClientHandler extends Thread {
                         if (field != null)
                             objectOutputStream.writeUTF(String.valueOf(field.isCompleted()));
                     }
+                    // Todo test
+                    case FLAG -> {
+                        if(field != null) {
+                            field.setFlag(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                            objectOutputStream.writeUTF(Serializer.externalToJson(field.getExternalCells()));
+                        }
+                    }
+                    // Todo test
+                    case SAVE_SCORE -> {
+                        new ScoreManager().add(args[1], LocalTime.parse(args[2], ScoreItem.timeFormatter));
+                        objectOutputStream.writeUTF("OK");
+                    }
+                    // Todo test
+                    case GET_TOP_USER -> objectOutputStream.writeUTF(Serializer.scoreItemToJson(new ScoreManager().getBest()));
                 }
 
                 objectOutputStream.flush();
-                // output.close();
 
                 System.err.println("Response was sent to " + socket.getInetAddress());
             }
